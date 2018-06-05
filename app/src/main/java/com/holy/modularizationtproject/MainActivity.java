a7e7ff579c035484e335913a447e601e2b26c4a4
holywang <holy0822@sina.com>
2018-06-05 18:12:43 +0800

    新建一个项目

--- app/src/main/java/com/holy/modularizationtproject/MainActivity.java
+++ app/src/main/java/com/holy/modularizationtproject/MainActivity.java
@@ -3,11 +3,17 @@ package com.holy.modularizationtproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.holy.dataprovider.network.NetworkHelper;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NetworkHelper helper = new NetworkHelper();
+
     }
 }
---
+++ dataprovider/src/main/java/com/holy/dataprovider/network/NetworkHelper.java
@@ -0,0 +1,10 @@
+package com.holy.dataprovider.network;
+
+/**
+ * Created by DR on 2018/6/5.
+ */
+
+public class NetworkHelper {
+
+
+}
