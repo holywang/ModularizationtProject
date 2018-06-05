ecc0845bac0bf44e0951682faf99b75250ae61fb
holywang <holy0822@sina.com>
2018-06-05 18:34:36 +0800

    添加Retrofit网络请求

--- dataprovider/src/main/java/com/holy/dataprovider/network/NetworkHelper.java
+++ dataprovider/src/main/java/com/holy/dataprovider/network/NetworkHelper.java
@@ -1,10 +1,21 @@
 package com.holy.dataprovider.network;
 
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by DR on 2018/6/5.
  */
 
 public class NetworkHelper {
 
-
+    public static <T> T network(String uri,Class<T> clz){
+        return new Retrofit.Builder()
+                .baseUrl(uri)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+                .create(clz);
+    }
 }
